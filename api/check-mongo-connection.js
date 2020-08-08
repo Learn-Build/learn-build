c0df0d261d1093b333c58f160b5b419ee25a3189
Alpri Else <alprielse@gmail.com>
2020-08-07 23:36:10 -0500

    Create create-user serverless function

---
+++ api/check-mongo-connection.js
@@ -0,0 +1,12 @@
+import connect from "../db/connect";
+
+module.exports = (req, res) => {
+  const db = connect().connection;
+
+  db.on("error", () => {
+    res.send("MongoDB connection error.");
+  });
+  db.once("open", () => {
+    res.send("MongoDB connected.");
+  });
+};
