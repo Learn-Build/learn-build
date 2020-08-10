b102f3825c234a7693f98420929329587467dad5
Renzo <renzol2@illinois.edu>
2020-08-10 13:08:01 -0500

    Apply static generation for tag routes

--- pages/tag/[tag].js
+++ pages/tag/[tag].js
@@ -1,7 +1,24 @@
-import { useRouter } from 'next/router';
+import { fetchTags } from '../api/client';
 
-export default function Tag() {
-  const router = useRouter();
-  const { tag } = router.query;  
-  return <h1>{tag}</h1>
+export default function Tag({id, name, description}) {
+  return (
+    <div>
+      <h1>{name}</h1>
+      <h2>Tag ID: {id}</h2>
+      <p>{description}</p>
+    </div>
+  );
+}
+
+export async function getStaticPaths() {
+  const tags = await fetchTags();
+  const paths = tags.map(tag => `/tag/${tag.name}`);
+  return { paths, fallback: false }
+}
+
+export async function getStaticProps(context) {
+  const tagName = context.params.tag;
+  const tags = await fetchTags();
+  const tagData = tags.find(t => t.name === tagName);
+  return {props: tagData};
 }
