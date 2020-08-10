e8d4f57a85a99eb3a4a8051ac3dba9044578040e
Renzo <renzol2@illinois.edu>
2020-08-10 13:31:20 -0500

    Apply SSG to build pages

--- pages/build/[name].js
+++ pages/build/[name].js
@@ -1,7 +1,24 @@
-import { useRouter } from 'next/router';
+import { fetchBuilds } from '../api/client';
 
-export default function Build() {
-  const router = useRouter();
-  const { name } = router.query;  
-  return <h1>{name}</h1>
+export default function Build({id, name, description}) {
+  return (
+    <div>
+      <h1>{name}</h1>
+      <h2>{`Build ID: ${id}`}</h2>
+      <p>{description}</p>
+    </div>
+  );
+}
+
+export async function getStaticPaths() {
+  const builds = await fetchBuilds();
+  const paths = builds.map(build => `/build/${build.name}`);
+  return { paths, fallback: false }
+}
+
+export async function getStaticProps(context) {
+  const buildName = context.params.name;
+  const builds = await fetchBuilds();
+  const buildData = builds.find(t => t.name === buildName);
+  return {props: buildData};
 }
