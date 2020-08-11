6d09cf3f89a22e429af6ded6271aa2defb885994
Renzo <renzol2@illinois.edu>
2020-08-11 13:40:27 -0500

    Edit fixtures

--- fixtures/builds.js
+++ fixtures/builds.js
@@ -1,26 +1,60 @@
+/*
+ * FIXME(Renzo): Hardcoding user/tag information for now. In the future, these
+ * will be ID'd so I"ll probably have to change some stuff maybe?
+ */
+
 export const BUILDS = [
   {
-    id: 0,
+    id: "0",
     name: "Introductory Deep Learning Build",
     description:
       "Learn the basics of deep learning, starting from nothing to finishing your first project.",
+    builder: "Renzo Ledesma",
+    tagIds: [
+      "4",
+      "5",
+      "6"
+    ],
+    likes: 54
   },
   {
-    id: 1,
+    id: "1",
     name: "Free Beginner to Advanced React Tutorial Build",
     description:
       "A curated collection of free online React resources to get you from complete beginner to React software engineer.",
+    builder: "Alpri Else",
+    tagIds: [
+      "7",
+      "0",
+      "8",
+      "9"
+    ],
+    likes: 32
   },
   {
-    id: 2,
+    id: "2",
     name: "UI UX Beginner Crash Course",
     description:
       "The best free video tutorials on getting into UI/UX design as a software engineer.",
+    builder: "John Doe",
+    tagIds: [
+      "10",
+      "11",
+      "12"
+    ],
+    likes: 78
   },
   {
-    id: 3,
+    id: "3",
     name: "Unity Intermediate Game Development Build",
     description:
       "Build upon Unity/C# fundamentals by following these video tutorials.",
+    builder: "Jackie Doe",
+    tagIds: [
+      "13",
+      "14",
+      "15"
+    ],
+    likes: 54
   },
 ];
