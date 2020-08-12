24cdbe033b6bcd466a2cab190ba53d7c97477abb
Renzo <renzol2@illinois.edu>
2020-08-11 19:59:25 -0500

    Add basic state to follow button

--- components/TagCard.js
+++ components/TagCard.js
@@ -1,8 +1,10 @@
+import { useState } from 'react';
 import { Box, Grid, Text, Button } from "@chakra-ui/core";
 import CardComponent from "./CardComponent";
 import LinkWrapper from "./LinkWrapper";
 
 function TagCard({ tag }) {
+  const [following, setFollowing] = useState(false);
 
   const desktopColumns = '80% 20%';
   const mobileColumns = '80% 20%';
@@ -15,22 +17,26 @@ function TagCard({ tag }) {
           <Box>
             <Text 
               as="h1" 
-              fontWeight="normal"
+              fontWeight="bold"
             >
               {tag.name}
             </Text>
 
             <Text 
               as="h2" 
-              fontWeight="light" 
+              fontWeight="normal" 
               isTruncated
             >
               {tag.description}
             </Text>
           </Box>
         </LinkWrapper>
-          <Button rightIcon="small-add">
-            Follow
+          <Button
+            rightIcon={following ? 'check' : 'small-add'}
+            onClick={() => setFollowing(!following)}
+            variant={following ? 'solid' : 'outline'}
+          >
+            {following ? 'Following' : 'Follow'}
           </Button>
       </Grid>
     </CardComponent>
