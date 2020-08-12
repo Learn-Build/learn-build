a003b0f65ca1e5264114449d5dc7fa6f1b36c639
Renzo <renzol2@illinois.edu>
2020-08-12 11:57:50 -0500

    Add basic styling and layout to build data

--- pages/build/[name].js
+++ pages/build/[name].js
@@ -1,16 +1,78 @@
-import { Heading, Text } from "@chakra-ui/core";
+import { Box, Heading, Text, Flex, Button, Image, Stack } from "@chakra-ui/core";
 import Container from "../../components/Container";
 import NavigationBar from "../../components/NavigationBar";
+import ResponsiveHeading from "../../components/ResponsiveHeading";
+import TogglableButton from "../../components/TogglableButton";
 import { fetchBuilds } from "../../clients";
 
-export default function Build({ id, name, description }) {
+export default function Build({ id, name, description, imageUrl=null }) {
+
+  const desktopWidth = 90;
+
   return (
     <div>
       <NavigationBar />
-      <Container>
-        <Heading>{name}</Heading>
-        <Text>{`Build ID: ${id}`}</Text>
-        <Text>{description}</Text>
+      <Flex
+        width={['95%', `${desktopWidth}%`]}
+        mx="auto"
+        maxH="200px"
+        flexDir="row"
+        borderWidth="1px"
+        boxShadow="sm"
+        px={5}
+        py={3}
+        my={5}
+      >
+        {/* Image */}
+        <Box flex={1}>
+          <Image 
+            src={imageUrl}
+            fallbackSrc="/assets/learn_build_logo.svg"
+            height="100%"
+          />
+        </Box>
+
+        {/* Title, description, tags */}
+        <Box flex={3}>
+          <Heading as="h1">{name}</Heading>
+          <Text>{description}</Text>
+        </Box>
+
+        {/* Likes, save buttons */}
+        <Box flex={1}>
+          <Stack
+            py={5}
+            px={5}
+            spacing={5}
+          >
+            <TogglableButton 
+              enabledText={'Favorited'}
+              disabledText={'Favorite'}
+              size="md"
+              props={{ mb: 3 }}
+            />
+            <TogglableButton 
+              enabledText={'Saved'}
+              disabledText={'Save'}
+              size="md"
+            />
+          </Stack>
+        </Box>
+      </Flex>
+      <Container desktopWidth={desktopWidth}>
+        {/* Resources */}
+        <Box>
+          <ResponsiveHeading>
+            Resources
+          </ResponsiveHeading>
+        </Box>
+
+        {/* Sidebar - notes and related builds? */}
+        <Box>
+          <ResponsiveHeading>
+            Notes
+          </ResponsiveHeading>
+        </Box>
       </Container>
     </div>
   );
