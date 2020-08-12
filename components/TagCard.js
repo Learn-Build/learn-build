6b9641c20d6296509e94064cf9354c20c160487d
Renzo <renzol2@illinois.edu>
2020-08-11 19:53:27 -0500

    Add follow button to tag card

--- components/TagCard.js
+++ components/TagCard.js
@@ -1,19 +1,39 @@
-import { Box } from "@chakra-ui/core";
+import { Box, Grid, Text, Button } from "@chakra-ui/core";
 import CardComponent from "./CardComponent";
 import LinkWrapper from "./LinkWrapper";
 
 function TagCard({ tag }) {
+
+  const desktopColumns = '80% 20%';
+  const mobileColumns = '80% 20%';
+  const responsiveCardColumns = [mobileColumns, mobileColumns, mobileColumns, desktopColumns];
+
   return (
-    <LinkWrapper href="/tags/[tag]" as={`/tags/${tag.name}`}>
-      <CardComponent>
-        <Box as="h1" fontWeight="normal">
-          {tag.name}
-        </Box>
-        <Box as="h2" fontWeight="light">
-          {tag.description}
-        </Box>
-      </CardComponent>
-    </LinkWrapper>
+    <CardComponent>
+      <Grid templateColumns={responsiveCardColumns}>
+        <LinkWrapper href="/tags/[tag]" as={`/tags/${tag.name}`}>
+          <Box>
+            <Text 
+              as="h1" 
+              fontWeight="normal"
+            >
+              {tag.name}
+            </Text>
+
+            <Text 
+              as="h2" 
+              fontWeight="light" 
+              isTruncated
+            >
+              {tag.description}
+            </Text>
+          </Box>
+        </LinkWrapper>
+          <Button rightIcon="small-add">
+            Follow
+          </Button>
+      </Grid>
+    </CardComponent>
   );
 }
 
