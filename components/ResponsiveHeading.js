64d53186ff280ed2bbadb8fa5913b1f11d4cbdad
Renzo <renzol2@illinois.edu>
2020-08-12 00:59:45 -0500

    Add filtering by builder

--- components/ResponsiveHeading.js
+++ components/ResponsiveHeading.js
@@ -1,14 +1,16 @@
 import { Heading } from "@chakra-ui/core";
 
-function ResponsiveHeading({ children }) {
+function ResponsiveHeading({ as="h2", size="xl", props, children }) {
   const responsiveAlign = ['center', 'center', 'center', 'left'];
 
   return (
     <Heading
-      as="h2"
+      as={as}
       mb={2}
       mt={4}
+      size={size}
       textAlign={responsiveAlign}
+      {...props}
     >
       {children}
     </Heading>
--- pages/search.js
+++ pages/search.js
@@ -2,20 +2,28 @@ import { useState } from "react";
 import { Icon, Input, InputGroup, Box, InputLeftElement } from "@chakra-ui/core";
 import NavigationBar from "../components/NavigationBar";
 import Container from "../components/Container";
+import CardComponent from "../components/CardComponent";
 import ResponsiveHeading from "../components/ResponsiveHeading";
 import Builds from "../components/Builds";
 import { fetchBuilds, fetchTags } from "../clients"
 
 export default function Search({ builds, tags }) {
-  const [query, setQuery] = useState('');
+  const [nameQuery, setNameQuery] = useState('');
+  const [builderQuery, setBuilderQuery] = useState('');
 
-  function handleQueryChange(event) {
-    setQuery(event.target.value);
+  function handleNameQueryChange(event) {
+    setNameQuery(event.target.value);
+  }
+
+  function handleBuilderQueryChange(event) {
+    setBuilderQuery(event.target.value);
   }
 
   function filterBuilds() {
     return builds.filter(build => (
-      build.name.toLowerCase().includes(query)
+      build.name.toLowerCase().includes(nameQuery)
+    )).filter(build => (
+      build.builder.toLowerCase().includes(builderQuery)
     ));
   }
 
@@ -25,6 +33,33 @@ export default function Search({ builds, tags }) {
       <Container leftColumn={30} rightColumn={70}>
         <Box>
           <ResponsiveHeading>Options</ResponsiveHeading>
+          <CardComponent>
+            <ResponsiveHeading
+              as={'h4'}
+              size={'md'}
+              props={{mt: 0}}
+            >
+              Filter by builder
+            </ResponsiveHeading>
+
+            <InputGroup>
+              <InputLeftElement children={<Icon name="search" color="gray.400" />} />
+              <Input
+                value={builderQuery}
+                placeholder="Search builder's name here..."
+                size="md"
+                onChange={handleBuilderQueryChange}  
+              />
+            </InputGroup>
+
+            <ResponsiveHeading
+              as={'h4'}
+              size={'md'}
+            >
+              Filter by topic
+            </ResponsiveHeading>
+            
+          </CardComponent>
         </Box>
 
         <Box>
@@ -32,10 +67,10 @@ export default function Search({ builds, tags }) {
           <InputGroup>
             <InputLeftElement children={<Icon name="search" color="gray.400" />} />
             <Input
-              value={query}
+              value={nameQuery}
               placeholder="Find a build to learn..."
               size="lg"
-              onChange={handleQueryChange}  
+              onChange={handleNameQueryChange}  
             />
           </InputGroup>
           <Builds builds={filterBuilds()} tags={tags} header={''} />
