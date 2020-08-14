aa99cc62952f77defaa910f9b40cdc48638002da
Kayla Raflores <kaylacr2@illinois.edu>
2020-08-14 13:12:36 -0500

    about page

--- components/AbSection.js
+++ components/AbSection.js
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
-import { Box, Heading, Grid, Image, Flex, Stack } from '@chakra-ui/core';
+import { Box, Heading, Grid, Image, Flex, Stack, Divider } from '@chakra-ui/core';
 import PropTypes from 'prop-types';
 import { Transition, CSSTransition } from 'react-transition-group';
+import AnimateHeight from 'react-animate-height';
 
-// About section icons
+// About/mission section icons
 function ImgIcon({ imgSrc, imgAlt }) {
   const duration = 300;
-
   const [isVisible, setIsVisible] = useState(false);
 
   // for making pretty
@@ -19,16 +19,15 @@ function ImgIcon({ imgSrc, imgAlt }) {
   }
 
   return (
-    <Flex background="grey">
+    <Flex background="grey"
+      onMouseOver={hoverState}
+      onMouseLeave={defaultState}>
       <Image
-        onMouseOver={hoverState}
-        onMouseLeave={defaultState}
-        style={{}}
         pr={30}
         src={imgSrc}
         alt={imgAlt}
-        htmlWidth={250}
-        htmlHeight={250}
+        htmlWidth={270}
+        htmlHeight={270}
       />
     </Flex>
   );
@@ -36,7 +35,7 @@ function ImgIcon({ imgSrc, imgAlt }) {
 
 // About section container w grid
 function AbSection({ headerText, subText, imgSrc1, imgAlt1, imgSrc2, imgAlt2, imgSrc3, imgAlt3 }) {
-  const horizontalGrid = '45% 55%';
+  const horizontalGrid = '40% 60%';
   const stackGrid = '100%';
   const responsiveGrid = [stackGrid, horizontalGrid, horizontalGrid, horizontalGrid];
 
@@ -59,7 +58,8 @@ function AbSection({ headerText, subText, imgSrc1, imgAlt1, imgSrc2, imgAlt2, im
   return (
     <Grid templateColumns={responsiveGrid}>
       <Stack
-        align="center">
+        align="center"
+        width="fill">
         <ImgIcon
           imgSrc={imgSrc1}
           imgAlt={imgAlt1}
@@ -76,7 +76,7 @@ function AbSection({ headerText, subText, imgSrc1, imgAlt1, imgSrc2, imgAlt2, im
 
       <Box
         alignSelf="center"
-        textAlign={['right', 'right', 'center', 'center']}>
+        textAlign={['left', 'left', 'left', 'left']}>
         <Heading
           as="h2"
           fontSize={h2FontSize}
@@ -95,6 +95,18 @@ function AbSection({ headerText, subText, imgSrc1, imgAlt1, imgSrc2, imgAlt2, im
           pt={[pvSm, pvSm, 2, 2]}
           pb={[pvLg, pvLg, 0, 0]}>
           {subText}
+        </Heading>
+
+        <Divider/>
+        <Heading
+          as="h3"
+          fontSize={h3FontSize}
+          fontWeight="normal"
+          pl={[phSm, phSm, phLg, phLg]}
+          pr={[phSm, phSm, 0, 0]}
+          pt={[pvSm, pvSm, 2, 2]}
+          pb={[pvLg, pvLg, 0, 0]}>
+        Our mission is to give control back to the user when it comes to deciding how they want to learn. With Learn Build, we hope to foster a community for learning and sharing open and accessible to everyone.
         </Heading>
       </Box>
     </Grid>
--- pages/about.js
+++ pages/about.js
@@ -2,43 +2,13 @@ import React from 'react';
 import { Heading, Divider, Box, Flex, Image, Text } from '@chakra-ui/core';
 import Hero from '../components/Hero';
 import AbSection from '../components/AbSection';
+import ValSection from '../components/ValSection';
+import PplSection from '../components/PplSection';
 import NavigationBar from '../components/NavigationBar';
 
-function PplIcon({ imgSrc, imgAlt, name, pos }) {
-  return (
-    <Flex>
-      <Image
-        pr={30}
-        src={imgSrc}
-        alt={imgAlt}
-        htmlWidth={300}
-        htmlHeight={300}
-        rounded="full"/>
-      <Text fontSize="3xl">{name}</Text>
-      <Text fontSize="2xl">{pos}</Text>
-    </Flex>
-  );
-}
-
 export default function About() {
-  const missionSt = "We are believers of a community built on learning. Learn Build exists as a greater space for sharing educational resources. We integrate self-learning with sharing and collaboration; users share and collaborate on 'builds' — educational pathways covering various topics.\n\nWe are the platform for providing others with resources to use so that they can share and learn from one another. Connecting learners with valuable online tools, courses, and other means to self-education is our goal.";
+  const missionSt = "We are believers of a community built on learning. Learn Build exists as a crowdsourcing platform for educational resources. We integrate self-learning with sharing and collaboration; users share and collaborate on 'builds' — educational pathways covering various topics.";
   const valueSt = "Learning, Sharing, Community";
-
-  // reminds me of h2/h3 rather than h1/h2 in Hero.js
-  const h2DesktopFont = '4vw';
-  const h2MobileFont = '8vw';
-  const h2FontSize = [h2MobileFont, h2MobileFont, h2DesktopFont, h2DesktopFont];
-
-  const h3DesktopFont = '2vw';
-  const h3MobileFont = '4vw';
-  const h3FontSize = [h3MobileFont, h3MobileFont, h3DesktopFont, h3DesktopFont];
-
-  // Constants for responsiveness
-  // Padding horizontal/vertical large/small
-  const phSm = 8;
-  const phLg = 20;
-  const pvSm = 6;
-  const pvLg = 10;
 
   return (
     <div>
@@ -50,7 +20,7 @@ export default function About() {
         heroImgAlt="Student making a build"
       />
 
-      {/* Mission section */}
+      {/* Mission section .. was gonna b more */}
       <Divider/>
       <AbSection
         headerText="Our Mission"
@@ -65,45 +35,13 @@ export default function About() {
 
       {/* Values section*/}
       <Divider/>
-      <AbSection
+      <ValSection
         headerText="Our Values"
-        subText={valueSt}
-        imgSrc1={"/assets/learning.png"}
-        imgAlt1={"Searching for interesting learning resources"}
-        imgSrc2={"/assets/sharing.png"}
-        imgAlt2={"Favoriting the best resources and builds"}
-        imgSrc3={"/assets/community.png"}
-        imgAlt3={"Creating learning builds for others to use"}>
-      </AbSection>
+        subText={valueSt}/>
 
       {/* Team section */}
-      {/* ..make diff class */}
       <Divider/>
-      <Box
-        alignSelf="center"
-        textAlign={['center', 'center', 'center', 'center']}>
-        <Heading
-          as="h2"
-          fontSize={h2FontSize}
-          pl={[phSm, phSm, phLg, phLg]}
-          pr={[phSm, phSm, 0, 0]}
-          pt={[pvSm, pvSm, 0, 0]}
-          pb={[0, 0, 0, 0]}>Our Team</Heading>
-        <Heading
-          as="h3"
-          fontSize={h3FontSize}
-          fontWeight="normal"
-          pl={[phSm, phSm, phLg, phLg]}
-          pr={[phSm, phSm, 0, 0]}
-          pt={[pvSm, pvSm, 2, 2]}
-          pb={[pvLg, pvLg, 0, 0]}>bebop</Heading>
-        <PplIcon
-          imgSrc={"/assets/3.png"}
-          imgAlt={"guy"}
-          name={"alpri else"}
-          pos={"pm"}
-        />
-      </Box>
+      <PplSection/>
     </div>
   );
 }
