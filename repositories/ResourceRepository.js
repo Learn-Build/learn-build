449f4cc179bd4a01da0f92ddfde940a88ae6be09
Chris Zhu <cthez72@gmail.com>
2020-08-14 01:19:14 -0500

    Adding all resource endpoint files

---
+++ repositories/ResourceRepository.js
@@ -0,0 +1,30 @@
+
+import Resource from '../models/resources'
+
+const ResourceRepository = {
+    getByTitle: async (title) => {
+      return new Promise((resolve, reject) => {
+        Resource.findOne({ title: title }, (err, resource) => {
+          if (err) {
+            reject(err)
+          }
+          resolve(resource)
+        })
+      })
+        
+    },
+    createResource: (resource) => {
+      return new Promise((resolve, reject) => {
+        const resourceModel = new Resource(resource)
+        resourceModel.save((err, resource) => {
+            if (err) {
+                reject(err)
+            }
+            resolve(resource)
+        })
+      })
+        
+    }
+}
+
+export default ResourceRepository
